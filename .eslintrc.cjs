a73802470f0e486f718559aaebd765177a004c4d
ClementMadiot <clementmadiot09@gmail.com>
2024-07-20 11:46:43 +0200

    add athentification, provider and routes

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,8 +9,35 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: { ecmaVersion: "latest", sourceType: "module" },
-  plugins: ["react-refresh"],
+  plugins: [
+    "react-refresh",
+    "@typescript-eslint",
+    "simple-import-sort",
+    "import",
+  ],
   rules: {
-    "react-refresh/only-export-components": "warn",
+    "react/prop-types": "off",
+    "react/jsx-no-target-blank": "off",
+    "react-refresh/only-export-components": [
+      "warn",
+      { allowConstantExport: true },
+    ],
+    "simple-import-sort/imports": [
+      "error",
+      {
+        groups: [
+          ["^react"],
+          ["^@refine"],
+          ["^@?\\w"],
+          ["^"],
+          ["^\\."],
+          ["^\\u0000"],
+        ],
+      },
+    ],
+    "simple-import-sort/exports": "error",
+    "import/first": "error",
+    "import/newline-after-import": "error",
+    "import/no-duplicates": "error",
   },
 };
--- src/providers/index.ts
+++ src/providers/index.ts
@@ -1,1 +1,2 @@
 export * from './data' // export everything come from data folder 
+export * from './auth' // export everything come from auth files 
